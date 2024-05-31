e95322477da5ca6750fb4cd7b639a71a19e3186a
hieunm <minhhieuittlu@gmail.com>
2024-05-31 14:39:26 +0700

    oke

---
+++ src/css/Common.js
@@ -0,0 +1,8 @@
+const myStyle = {
+  color: "white",
+  backgroundColor: "DodgerBlue",
+  padding: "10px",
+  fontFamily: "Sans-Serif",
+};
+
+export default myStyle;
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import styles from "../css/App.css";
+import mySass from "../scss/my-sass.scss";
+
+const Home = () => {
+  return <h1 className={styles.bigblue}>Home</h1>;
+};
+
+export default Home;
